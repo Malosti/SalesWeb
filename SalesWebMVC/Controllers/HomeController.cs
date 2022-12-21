dcfa79c09e74cab8ca69fc78920b17e7fd4f4bea
Guilherme Malosti Fonseca <guilherme.fonseca@gsw.com.br>
2022-12-21 10:52:43 -0300

    First Controller e Razor test

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MCV App from C# course.";
+            ViewData["Aluno"] = "Guilherme Malosti Fonseca";
 
             return View();
         }
