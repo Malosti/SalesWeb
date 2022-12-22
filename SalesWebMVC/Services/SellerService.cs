edb16be6f18cf70b078761d4dca1d654b18ea78e
Guilherme Malosti Fonseca <guilherme.fonseca@gsw.com.br>
2022-12-22 11:43:24 -0300

    Seller Service and Find All

---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,26 @@
+﻿using SalesWebMVC.Model;
+using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        // Readonly é uma pratica quando se deseja que o arquivo sirva apenas para leitura, não podendo ser alterado
+        private readonly SalesWebMVCContext _context; 
+
+        public SellerService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            // acessa a fonte de dados da tabela vendedores e converte em uma lista
+            return _context.Seller.ToList();
+        }
+    }
+}
