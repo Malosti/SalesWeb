45c421640152c5d21eb41469d6ab992a639353c7
Guilherme Malosti Fonseca <guilherme.fonseca@gsw.com.br>
2022-12-23 12:20:07 -0300

    SellerFormViewModel and Department select component

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -12,10 +12,12 @@ namespace SalesWebMVC.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -27,7 +29,9 @@ namespace SalesWebMVC.Controllers
         // Este metodo create apenas apresenta a tela de cadastro (GET)
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
