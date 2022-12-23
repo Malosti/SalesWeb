fb3efa1935a2da9f7124facab668b791cf3696f6
Guilherme Malosti Fonseca <guilherme.fonseca@gsw.com.br>
2022-12-23 14:02:31 -0300

    Update seller and custom service exception

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -5,13 +5,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Services.Exceptions;
 
 namespace SalesWebMVC.Services
 {
     public class SellerService
     {
         // Readonly é uma pratica quando se deseja que o arquivo sirva apenas para leitura, não podendo ser alterado
-        private readonly SalesWebMVCContext _context; 
+        private readonly SalesWebMVCContext _context;
 
         public SellerService(SalesWebMVCContext context)
         {
@@ -40,6 +41,23 @@ namespace SalesWebMVC.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
